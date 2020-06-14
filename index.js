e359923dd5d708c8809a1db7826ae59193180d72
Sebastian Prein <hi@sebastianprein.com>
2020-06-14 15:22:21 +0200

    fix: duplicates not being spread

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 /**
+ * Merges two objects together and automatically
+ * creates arrays for entries that are duplicates.
+ *
+ * @param {object} a Object a.
+ * @param {object} b Object b.
+ * @returns {object}
+ */
+function merge(a, b) {
+  const result = { ...a };
+
+  Object
+    .entries(b)
+    .forEach(([bId, bValue]) => {
+      const aValue = result[bId];
+
+      if (aValue === undefined) {
+        result[bId] = bValue;
+      } else {
+        result[bId] = [
+          ...(Array.isArray(aValue) ? aValue : [aValue]),
+          ...(Array.isArray(bValue) ? bValue : [bValue]),
+        ];
+      }
+    });
+
+  return result;
+}
+
+/**
  * Creates a ShadowDOM for this element and
  * uses the given template as content.
  *
@@ -7,15 +36,15 @@
  * @param {ShadowRootInit} settings Shadow DOM settings.
  */
 export function createShadowRoot($el, template, settings = { mode: 'open' }) {
-    const shadowRoot = $el.attachShadow(settings);
+  const shadowRoot = $el.attachShadow(settings);
 
-    shadowRoot.appendChild(
-        (new DOMParser())
-            .parseFromString(template, 'text/html')
-            .querySelector('template')
-            .content
-            .cloneNode(true),
-    );
+  shadowRoot.appendChild(
+    (new DOMParser())
+      .parseFromString(template, 'text/html')
+      .querySelector('template')
+      .content
+      .cloneNode(true),
+  );
 }
 
 /**
@@ -27,19 +56,19 @@ export function createShadowRoot($el, template, settings = { mode: 'open' }) {
  * @param {CustomEventInit} settings Event settings.
  */
 export function dispatch($el, name, detail, settings = {}) {
-    const defaults = {
-        bubbles: true,
-        composed: false,
-        cancelable: false,
-    };
+  const defaults = {
+    bubbles: true,
+    composed: false,
+    cancelable: false,
+  };
 
-    $el.dispatchEvent(
-        new CustomEvent(name, {
-            detail,
-            ...defaults,
-            ...settings
-        })
-    );
+  $el.dispatchEvent(
+    new CustomEvent(name, {
+      detail,
+      ...defaults,
+      ...settings,
+    }),
+  );
 }
 
 /**
@@ -50,25 +79,25 @@ export function dispatch($el, name, detail, settings = {}) {
  * @return {Attr[]} Returns a list of matching attributes.
  */
 export function findAttributes($el, name) {
-    if (name instanceof RegExp === false) {
-        throw new Error('Attribute name must be a regular expression.');
-    }
+  if (name instanceof RegExp === false) {
+    throw new Error('Attribute name must be a regular expression.');
+  }
 
-    const attributes = [];
+  const attributes = [];
 
-    if ($el.attributes === null || $el.attributes === undefined || typeof $el.attributes[Symbol.iterator] !== 'function') {
-        return attributes;
-    }
-
-    for (let i = 0; i < $el.attributes.length; i += 1) {
-        const attribute = $el.attributes[i];
-
-        if (name.test(attribute.name)) {
-            attributes.push(attribute);
-        }
-    }
-
+  if ($el.attributes === null || $el.attributes === undefined || typeof $el.attributes[Symbol.iterator] !== 'function') {
     return attributes;
+  }
+
+  for (let i = 0, n = $el.attributes.length; i < n; i += 1) {
+    const attribute = $el.attributes[i];
+
+    if (name.test(attribute.name)) {
+      attributes.push(attribute);
+    }
+  }
+
+  return attributes;
 }
 
 /**
@@ -76,44 +105,41 @@ export function findAttributes($el, name) {
  *
  * @param {HTMLElement} $el Element to look for references.
  * @param {object} settings Settings for adjusting behaviour.
- * @param {object} $refs Merge with existing references.
  * @returns {object}
  */
-export function findReferences($el, settings = {}, $refs = {}) {
-    const defaults = {
-        pattern: /^#(?<id>.+)/,
-        recursive: true,
-    };
+export function findReferences($el, settings = {}) {
+  const defaults = {
+    pattern: /^#(?<id>.+)/,
+    recursive: true,
+    cleanUp: true,
+  };
 
-    const { pattern, ignoreDuplicates, recursive } = { ...defaults, ...settings };
+  const $refs = {};
+  const { pattern, recursive, cleanUp } = { ...defaults, ...settings };
 
-    if ($el.childElementCount > 0 && recursive) {
-        for (let i = 0; i < $el.childElementCount; i += 1) {
-            findReferences($el.children[i], settings, $refs);
-        }
+  if (recursive) {
+    for (let i = 0, n = $el.childElementCount; i < n; i += 1) {
+      merge($refs, findReferences($el.children[i], settings));
     }
+  }
 
-    const [ref] = findAttributes($el, pattern);
+  const [ref] = findAttributes($el, pattern);
 
-    if (ref === undefined) {
-        return $refs;
-    }
-
-    const { id } = ref.name.match(pattern).groups || {};
-
-    if (id === undefined) {
-        throw new Error('Reference pattern must include named group “id”.');
-    }
-
-    if (Array.isArray($refs[id])) {
-        $refs[id].push($el);
-    } else {
-        $refs[id] = $refs[id] === undefined ? $el : [$refs[id], $el];
-    }
-
-    $el.removeAttribute(ref.name);
-
+  if (ref === undefined) {
     return $refs;
+  }
+
+  const { id } = ref.name.match(pattern).groups || {};
+
+  if (id === undefined) {
+    throw new Error('Reference pattern must include named group “id”.');
+  }
+
+  if (cleanUp) {
+    $el.removeAttribute(ref.name);
+  }
+
+  return merge($refs, { [id]: $el });
 }
 
 /**
@@ -123,20 +149,22 @@ export function findReferences($el, settings = {}, $refs = {}) {
  * @returns {boolean}
  */
 export function isElementInViewport($el) {
-    const { top, left, bottom, right } = $el.getBoundingClientRect();
+  const {
+    top, left, bottom, right,
+  } = $el.getBoundingClientRect();
 
-    return (
-        top >= 0 &&
-        left >= 0 &&
-        bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+  return (
+    top >= 0
+        && left >= 0
+        && bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        && right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
 
 export default {
-    createShadowRoot,
-    dispatch,
-    findAttributes,
-    findReferences,
-    isElementInViewport,
+  createShadowRoot,
+  dispatch,
+  findAttributes,
+  findReferences,
+  isElementInViewport,
 };
