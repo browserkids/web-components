4b37664e5e09ab09b9ba41a1ac7f850531d7d7ff
Sebastian Prein <hi@sebastianprein.com>
2020-06-19 22:08:34 +0200

    refactor: use treewalke for bindEventListeners

--- index.js
+++ index.js
@@ -151,61 +151,59 @@ export function isElementInViewport($el) {
 export function bindEventListeners($el, scope = $el, settings = {}) {
   const defaults = {
     pattern: /^@(?<event>[^.]+).?(?<modifier>.+)?/,
-    recursive: true,
     cleanUp: true,
   };
 
-  const { pattern, recursive, cleanUp } = { ...defaults, ...settings };
+  const { pattern, cleanUp } = { ...defaults, ...settings };
+  const walker = document.createTreeWalker($el, NodeFilter.SHOW_ELEMENT);
 
-  if (recursive) {
-    for (let i = 0, n = $el.childElementCount; i < n; i += 1) {
-      bindEventListeners($el.children[i], scope, settings);
-    }
-  }
+  do {
+    const $currentNode = walker.currentNode;
 
-  findAttributes($el, pattern).forEach((attribute) => {
-    const { event, modifier = '' } = attribute.name.match(pattern).groups || {};
+    findAttributes($currentNode, pattern).forEach((attribute) => {
+      const { event, modifier = '' } = attribute.name.match(pattern).groups || {};
 
-    if (event === undefined) {
-      throw new Error('Reference pattern must include named group “event”.');
-    }
-
-    const fn = scope[attribute.value];
-
-    if (typeof fn !== 'function') {
-      throw new Error(`Event listener function “${attribute.value}” is invalid or undefined for this element.`);
-    }
-
-    if (cleanUp) {
-      $el.removeAttribute(attribute.name);
-    }
-
-    const $target = (modifier.includes('window') && window)
-      || (modifier.includes('document') && document)
-      || (modifier.includes('away') && window)
-      || $el;
-
-    const handler = (e) => {
-      if (
-        (modifier.includes('self') && $el !== e.target)
-        || (modifier.includes('away') && $el.contains(e.target))
-      ) {
-        return;
+      if (event === undefined) {
+        throw new Error('Reference pattern must include named group “event”.');
       }
 
-      if (modifier.includes('prevent')) {
-        e.preventDefault();
+      const fn = scope[attribute.value];
+
+      if (typeof fn !== 'function') {
+        throw new Error(`Event listener function “${attribute.value}” is invalid or undefined for this element.`);
       }
 
-      fn.call(scope, e);
-
-      if (modifier.includes('once')) {
-        $target.removeEventListener(event, handler);
+      if (cleanUp) {
+        $currentNode.removeAttribute(attribute.name);
       }
-    };
 
-    $target.addEventListener(event, handler);
-  });
+      const $target = (modifier.includes('window') && window)
+        || (modifier.includes('document') && document)
+        || (modifier.includes('away') && window)
+        || $currentNode;
+
+      const handler = (e) => {
+        if (
+          (modifier.includes('self') && $currentNode !== e.target)
+          || (modifier.includes('away') && $currentNode.contains(e.target))
+        ) {
+          return;
+        }
+
+        if (modifier.includes('prevent')) {
+          e.preventDefault();
+        }
+
+        fn.call(scope, e);
+
+        if (modifier.includes('once')) {
+          $target.removeEventListener(event, handler);
+        }
+      };
+
+      $target.addEventListener(event, handler);
+    });
+  } while (walker.nextNode() !== null);
 }
 
 /**
