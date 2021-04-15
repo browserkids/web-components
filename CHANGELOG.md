8574900c11d922deab59c49bfae2d6349bd9504c
Sebastian Prein <hi@sebastianprein.com>
2021-04-16 00:06:37 +0200

    release: 0.4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,12 +4,21 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [0.4.0] - 2021-04-16
+### Added
+- Module entry-point
+- Terser for minification
+- Dependency related single-imports
+- Mark this package as side-effects free
+
+### Changed
+- It’s at least es9 support required
+- Allow single imports
 
 
 ## [0.3.0] - 2020-06-23
 ### Added
-- Adjust behaviour of `isElementInViewport()` with `amount` and `viewport` settings 
+- Adjust behaviour of `isElementInViewport()` with `amount` and `viewport` settings
 
 ### Changed
 - Return created shadow root when calling `createShadowRoot()`
@@ -18,7 +27,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [0.2.0] - 2020-06-14
 ### Added
-- Exporting all functions as object via default export 
+- Exporting all functions as object via default export
 - New `bindEventListeners` function
 - New `findReferences` function
 
@@ -29,7 +38,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [0.1.0] - 2020-06-13
 First silent release.
 
-[Unreleased]: https://github.com/browserkids/dom/compare/0.3.0...HEAD
-[0.3.0]: https://github.com/browserkids/dom/releases/tag/0.3.0
-[0.2.0]: https://github.com/browserkids/dom/releases/tag/0.2.0
+[Unreleased]: https://github.com/browserkids/dom/compare/0.4.0...HEAD
+[0.4.0]: https://github.com/browserkids/dom/compare/0.3.0...0.4.0
+[0.3.0]: https://github.com/browserkids/dom/compare/0.2.0...0.3.0
+[0.2.0]: https://github.com/browserkids/dom/compare/0.1.0...0.2.0
 [0.1.0]: https://github.com/browserkids/dom/releases/tag/0.1.0
--- bin/pack.js
+++ bin/pack.js
@@ -1,39 +1,49 @@
 #!/usr/bin/env node
-const fs = require('fs').promises;
-const path = require('path');
+const { readdir, readFile, writeFile } = require('fs').promises;
+const { resolve, basename } = require('path');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
 const terser = require('terser');
 
 const { name, version } = require('../package.json');
 
-const root = path.resolve(__dirname, '..');
-const sourceDirectory = path.resolve(root, 'src');
+const root = resolve(__dirname, '..');
+const sourceDirectory = resolve(root, 'src');
 const targetDirectory = root;
-const targetPath = path.resolve(root, 'index.js');
+const targetPath = resolve(root, 'index.js');
 
-function minify(code) {
-  return terser.minify(code, { ecma: 9 }).code;
+async function minify(code) {
+  return (await terser.minify(code, { ecma: 9 })).code;
 }
 
 (async () => {
-  const sourcePaths = await fs.readdir(sourceDirectory);
-  const sourceFiles = await Promise.all(sourcePaths.map((filePath) => fs.readFile(path.resolve(sourceDirectory, filePath))));
-  const sourceNames = sourcePaths.map((item) => path.basename(item, '.js'));
+  const sourcePaths = await readdir(sourceDirectory);
+  const sourceFiles = await Promise.all(sourcePaths.map((filePath) => readFile(resolve(sourceDirectory, filePath))));
+  const sourceNames = sourcePaths.map((item) => basename(item, '.js'));
 
   let contents = '';
 
   console.log('📦  Processing files…\n');
 
-  sourcePaths.forEach((sourcePath, index) => {
-    const code = sourceFiles[index].toString();
+  await Promise.all(
+    sourcePaths.map(async (sourcePath, index) => {
+      const code = sourceFiles[index].toString();
 
-    contents += code.replace(/export default/g, 'export');
+      contents += code.replace(/export default/g, 'export');
 
-    fs.writeFile(path.resolve(targetDirectory, sourcePath), minify(`/*! ${name}@${version} */${code}`));
+      await writeFile(
+        resolve(targetDirectory, sourcePath),
+        await minify(`/*! ${name}@${version} */${code}`),
+      );
 
-    console.log(`✓ ${sourcePath}`);
-  });
+      console.log(`✓ ${sourcePath}`);
+    }),
+  );
 
-  await fs.writeFile(targetPath, minify(`/*! ${name}@${version} */${contents} export default { ${sourceNames} }`));
+  await writeFile(
+    targetPath,
+    await minify(`/*! ${name}@${version} */${contents} export default { ${sourceNames} }`),
+  );
 
   console.log('✓ index.js');
   console.log('\n\n👏  Done.\n');
